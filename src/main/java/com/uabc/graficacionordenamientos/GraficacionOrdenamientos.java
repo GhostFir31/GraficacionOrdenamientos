95bb2f34205e35072e3434aa7239f701cb7c28c6
GhostFir31 <j3sus0marle@gmail.com>
2023-11-12 18:30:46 -0800

    HUD

---
+++ src/main/java/com/uabc/graficacionordenamientos/GraficacionOrdenamientos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uabc.graficacionordenamientos;
+
+/**
+ *
+ * @author JesusOmar Leal
+ */
+public class GraficacionOrdenamientos {
+
+    public static void main(String[] args) {
+        
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);  
+        
+    }
+}
